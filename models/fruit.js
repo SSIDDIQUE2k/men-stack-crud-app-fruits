50aa0e58bbcb3d3e1270b5760be7d21006fd9270
Shazib Siddique <siddiqueshazib4@gmail.com>
2024-06-21 12:53:29 -0400

    complete fruit create work from shazib

---
+++ models/fruit.js
@@ -0,0 +1,11 @@
+const { name } = require('ejs');
+const  mongoose = require('mongoose');
+
+const fruitSchema = new mongoose.Schema({
+    name: String,
+    isReadyToEat: Boolean,
+});
+
+const Fruit = mongoose.model('Fruit', fruitSchema);
+
+module.exports = Fruit;
